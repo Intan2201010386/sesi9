fab272b15560a7bc7a91ba2034fe00f45c56e3a8
Intan2201010386 <127836584+Intan2201010386@users.noreply.github.com>
2023-05-09 17:03:43 +0800

    Add files via upload
    Upload

---
+++ sesi9/javascript02.js
@@ -0,0 +1,2 @@
+document.getElementById("teks1").style.color = "#FF8AC8"
+document.getElementById("teks2").style.color = "#FF8A8A"
---
+++ sesi9/javascript02.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 2</title>
+</head>
+<body>
+    
+<div id="teks1">
+    <p>Hari ini selasa 9 Mei saya dan teman sekelas saya bersama-sama belajar script javascript</p>
+</div>
+<div id="teks2">
+    <p>Latihan ini untuk belajar script javascript</p>
+</div>
+
+    <script src="javascript02.js"></script>
+
+</body>
+</html>
